020463dbf8834b39ce02be2c47ec33b78084c1b7
ndf-zz <ndf-zz@6-v.org>
2025-07-20 13:05:20 +1000

    Use XDG name for icon and desktop file and preload audio on startup

--- src/ttstart/__init__.py
+++ src/ttstart/__init__.py
@@ -31,6 +31,7 @@
        # apt remove 'pulseaudio*'
 
 """
+__version__ = '1.0.2a1'
 
 import sys
 import gi
@@ -63,18 +64,21 @@ from gi.repository import PangoCairo
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 
-VERSION = '1.0.1'
+PRGNAME = 'org.6_v.ttstart'
+APPNAME = 'TTStart'
+
 _DEFTOPIC = 'startlist'
-_DEFAUDIOSYNCTHRESH = 0.12
+_DEFAUDIOSYNCTHRESH = 0.15
 _RESOURCE_PKG = 'ttstart'
 _SOUNDFILE = 'start.wav'
 _STARTLIST = 'startlist.csv'
 _TIMEFONT = 'TeXGyreHerosCn Bold '
 _COUNTERFONT = 'TeXGyreHerosCn Bold '
-_NOHDR = [u'Start', u'start', u'time', u'Time', u'']
+_NOHDR = ['Start', 'start', 'time', 'Time', '']
 _LOGLEVEL = logging.DEBUG
 _log = logging.getLogger('ttstart')
 _log.setLevel(_LOGLEVEL)
+_PANGO_SCALE = float(Pango.SCALE)
 
 
 def _json_hook(obj):
@@ -86,37 +90,20 @@ def _json_hook(obj):
     return obj
 
 
-def _fit_cent(cr, pr, h, w, msg, charHeight, fontName):
-    """Squeeze-fit msg centered at h in a box of width w."""
+def _fit_cent(cr, pr, h, w, msg, charHeight, fontName, pagew, ellipsize=True):
+    """Fit msg centered on page at h in a box of width w."""
     if msg is not None:
         cr.save()
         l = Pango.Layout.new(pr)
         l.set_alignment(Pango.Alignment.CENTER)
+        if ellipsize:
+            l.set_ellipsize(Pango.EllipsizeMode.END)
         l.set_font_description(
             Pango.FontDescription(fontName + str(charHeight)))
         l.set_text(msg, -1)
-        (tw, th) = l.get_pixel_size()
-        if tw > w:
-            cr.scale(float(w) / float(tw), 1.0)
-            tw = w
-        cr.move_to(0.5 * (w - tw), h)
-        PangoCairo.update_context(cr, pr)
-        l.context_changed()
-        PangoCairo.show_layout(cr, l)
-        cr.restore()
-
-
-def _text_cent(cr, pr, h, w, msg, charHeight, fontName):
-    """Position text centered at h,w."""
-    if msg is not None:
-        cr.save()
-        l = Pango.Layout.new(pr)
-        l.set_alignment(Pango.Alignment.CENTER)
-        l.set_font_description(
-            Pango.FontDescription(fontName + str(charHeight)))
-        l.set_text(msg, -1)
-        (tw, th) = l.get_pixel_size()
-        cr.move_to(w - (0.5 * tw), h)
+        l.set_width(int(w * _PANGO_SCALE))
+        l.set_height(0)
+        cr.move_to(0.5 * (pagew - w), h)
         PangoCairo.update_context(cr, pr)
         l.context_changed()
         PangoCairo.show_layout(cr, l)
@@ -149,6 +136,7 @@ class ttstart(Gtk.Window):
                                   Gst.ElementFactory.make('alsasink', 'sink'))
         self._player.set_property(
             'video-sink', Gst.ElementFactory.make('fakesink', 'fakesink'))
+
         # there's a 32 bit overflow somewhere in gst, for now ignore the bus
         #bus = self._player.get_bus()
         #bus.add_signal_watch()
@@ -180,7 +168,7 @@ class ttstart(Gtk.Window):
         self._area = Gtk.DrawingArea()
         self._area.connect('configure-event', self._configure)
         self._area.connect('draw', self._draw)
-        self._area.set_size_request(512, 300)
+        self._area.set_size_request(400, 300)
         self.add(self._area)
         self._area.show()
 
@@ -193,8 +181,6 @@ class ttstart(Gtk.Window):
         self._loadconfig()
         if self._topic:
             self._io.subscribe(self._topic)
-        with metarace.resource_file(metarace.LOGO) as f:
-            Gtk.Window.set_default_icon_from_file(str(f))
         self.show()
         self._tod = tod.now().truncate(0)
         self._nc = self._tod + tod.tod('1.21')
@@ -207,7 +193,7 @@ class ttstart(Gtk.Window):
         _log.debug('Handle destroy cb')
         self._running = False
         self.hide()
-        self._player.set_state(Gst.State.NULL)
+        self._cancel_audio()
         self._player = None
         self._io.exit()
         Gtk.main_quit()
@@ -274,8 +260,8 @@ class ttstart(Gtk.Window):
         if self._tod is not None:
             oh = -0.02 * self._height
             ch = 0.12 * self._height
-            _text_cent(cr, pr, oh, 0.5 * self._width, self._tod.meridiem(), ch,
-                       _TIMEFONT)
+            _fit_cent(cr, pr, oh, 0.95 * self._width, self._tod.meridiem(), ch,
+                      _TIMEFONT, self._width, False)
 
         # countdown txt
         if self._countdown is not None:
@@ -286,14 +272,15 @@ class ttstart(Gtk.Window):
                 ctx = '+' + str(-self._countdown)
             oh = 0.04 * self._height
             ch = 0.46 * self._height
-            _text_cent(cr, pr, oh, 0.5 * self._width, ctx, ch, _COUNTERFONT)
+            _fit_cent(cr, pr, oh, 0.95 * self._width, ctx, ch, _COUNTERFONT,
+                      self._width, False)
 
         # rider name txt
         if self._riderstr is not None:
             oh = 0.78 * self._height
             ch = 0.10 * self._height
-            _fit_cent(cr, pr, oh, self._width, self._riderstr, ch,
-                      _COUNTERFONT)
+            _fit_cent(cr, pr, oh, 0.95 * self._width, self._riderstr, ch,
+                      _COUNTERFONT, self._width)
 
         # starter bulbs
         if self._bulb is not None:
@@ -330,7 +317,7 @@ class ttstart(Gtk.Window):
             }
         })
         cr.add_section('ttstart')
-        cr.merge(metarace.sysconf, u'ttstart')
+        cr.merge(metarace.sysconf, 'ttstart')
 
         # set fullscreen
         if cr.get_bool('ttstart', 'fullscreen'):
@@ -382,7 +369,7 @@ class ttstart(Gtk.Window):
                 _log.debug('Wrote audio file %r', audioFile)
         else:
             _log.debug('Using %r for audio file', audioFile)
-        self._player.set_property('uri', u'file://' + audioFile)
+        self._player.set_property('uri', 'file://' + audioFile)
 
         # load riders (may be from CWD)
         datafile = metarace.default_file(cr.get('ttstart', 'startlist'))
@@ -397,7 +384,7 @@ class ttstart(Gtk.Window):
                         bib = ''
                         series = ''
                         name = ''
-                        # load rider info
+                        # load rider info (ignore cat)
                         # start, no, series, name, cat
                         if len(r) > 0 and r[0] not in _NOHDR:
                             st = tod.mktod(r[0])
@@ -410,7 +397,7 @@ class ttstart(Gtk.Window):
                             if st is not None:
                                 # enough data to add a starter
                                 count += 1
-                                nr = [st, bib, series, name]
+                                nr = (st, bib, series, name)
                                 rlist.append(nr)
                             else:
                                 _log.warning('Ignored invalid starter %r', r)
@@ -420,6 +407,11 @@ class ttstart(Gtk.Window):
                 _log.warning('%s loading from startlist: %s',
                              e.__class__.__name__, e)
 
+    def _cancel_audio(self):
+        """Force audio player to null state"""
+        self._player.set_state(Gst.State.NULL)
+        return False
+
     def _delayed_cursor(self):
         """Remove the mouse cursor from the text area and go fullscreen"""
         cursor = Gdk.Cursor.new_for_display(Gdk.Display.get_default(),
@@ -427,30 +419,32 @@ class ttstart(Gtk.Window):
         self.get_window().set_cursor(cursor)
         if self._fullscreen:
             self.fullscreen()
+
+        # run a start signal sanity check
+        self._player.set_state(Gst.State.PLAYING)
+        GLib.timeout_add_seconds(2, self._cancel_audio)
         return False
 
     def _check_play_pos(self, expect):
-        """Check and handle audio desync error."""
+        """Check and handle audio offset."""
         try:
             postype, curpos = self._player.query_position(Gst.Format.TIME)
             if postype:
                 err = float(abs(expect - curpos)) * 1e-9
                 if err > self._syncthresh:
-                    _log.error('Audio desync ~%0.3fs', err)
-                    self._player.set_state(Gst.State.NULL)
+                    _log.error('Audio offset ~%0.3fs', err)
+                    self._cancel_audio()
                 else:
                     pass
                     #_log.debug('Audio offset ~%0.3fs', err)
         except Exception as e:
-            _log.error('%s reading audio position: %s', e.__class__.__name__,
-                       e)
+            _log.error('%s reading audio offset: %s', e.__class__.__name__, e)
         return False
 
     def _timeout(self, data=None):
         """Handle timeout"""
         if not self._running:
             return False
-
         try:
             ntime = tod.now()
             ntod = ntime.truncate(0)
@@ -470,12 +464,11 @@ class ttstart(Gtk.Window):
             if tod.MAX - tt < tod.ONE:
                 _log.debug('Midnight rollover')
                 break
-            _log.debug('May have missed an interval, catching up')
-            self._nc += tod.ONE  # 0.01 allows for processing delay
+            _log.debug('Missed an interval, catching up')
+            self._nc += tod.ONE
         ival = int(1000.0 * float((self._nc - tod.now()).timeval))
         GLib.timeout_add(ival, self._timeout)
-
-        return False  # must return False
+        return False
 
     def _set_backlight(self, percent=None):
         """Attempt to adjust screen brightness between riders."""
@@ -528,7 +521,7 @@ class ttstart(Gtk.Window):
                 self._set_backlight(self._backlightlow)
                 self._clear()  # note also removes the bulb
                 self._currider = self._ridermap[self._currider][4]
-                self._player.set_state(Gst.State.NULL)
+                self._cancel_audio()
             if cdn >= 0 and cdn <= 30:
                 if self._bulb:
                     self._countdown = cdn
@@ -549,14 +542,14 @@ class ttstart(Gtk.Window):
         """Handle a Gst bus message"""
         t = message.type
         if t == Gst.MessageType.EOS:
-            self._player.set_state(Gst.State.NULL)
+            self._cancel_audio()
+            _log.debug('gst EOS')
         elif t == Gst.MessageType.ERROR:
-            self._player.set_state(Gst.State.NULL)
+            self._cancel_audio()
             err, debug = message.parse_error()
             _log.error('gst error: %r/%r', err, debug)
         else:
             pass
-        #_log.debug('gst message: %r', message)
 
     def _newStartlist(self, startlist=[]):
         """Re-load the startlist"""
@@ -634,6 +627,14 @@ def main():
     fh = logging.Formatter(metarace.LOGFORMAT)
     ch.setFormatter(fh)
     logging.getLogger().addHandler(ch)
+
+    try:
+        GLib.set_prgname(PRGNAME)
+        GLib.set_application_name(APPNAME)
+        Gtk.Window.set_default_icon_name(metarace.ICON)
+    except Exception as e:
+        _log.debug('%s setting property: %s', e.__class__.__name__, e)
+
     metarace.init()
     Gst.init()
     app = ttstart()
